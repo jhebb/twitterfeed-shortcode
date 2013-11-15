c7eb3aa8ea3fc4a817d783664415ffcac095cf79
Justin Hebb <justinhebb@gmail.com>
2013-11-14 22:49:59 -0400

    initial commit

---
+++ twitter-feed-settings.php
@@ -0,0 +1,142 @@
+<?php
+class TwitterFeedSettings
+{
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
+
+    /**
+     * Start up
+     */
+    public function __construct()
+    {
+        add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+        add_action( 'admin_init', array( $this, 'page_init' ) );
+    }
+
+    /**
+     * Add options page
+     */
+    public function add_plugin_page()
+    {
+        // This page will be under "Settings"
+        add_options_page(
+            'Twitter Feed Settings',
+            'Twitter Feed',
+            'manage_options',
+            'twitterfeed-admin',
+            array( $this, 'create_admin_page' )
+        );
+    }
+
+    /**
+     * Options page callback
+     */
+    public function create_admin_page()
+    {
+        // Set class property
+        $this->options = get_option( 'twitterfeed_settings' );
+        ?>
+        <div class="wrap">
+            <?php screen_icon(); ?>
+            <h2>Twitter Feed Settings</h2>
+            <form method="post" action="options.php">
+            <?php
+                // This prints out all hidden setting fields
+                settings_fields( 'twitterfeed_settings' );
+                do_settings_sections( 'twitterfeed-admin' );
+                submit_button();
+            ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function page_init()
+    {
+        register_setting(
+            'twitterfeed_settings', // Option group
+            'twitterfeed_settings', // Option name
+            array( $this, 'sanitize' ) // Sanitize
+        );
+
+        add_settings_section(
+            'twitter_api_keys', // ID
+            'Twitter API Keys', // Title
+            array( $this, 'twitter_api_keys_callback' ), // Callback
+            'twitterfeed-admin' // Page
+        );
+
+        add_settings_field(
+            'consumer_key', // ID
+            'Consumer Key', // Title
+            array( $this, 'consumer_key_callback' ), // Callback
+            'twitterfeed-admin', // Page
+            'twitter_api_keys' // Section
+        );
+
+        add_settings_field(
+            'consumer_secret',
+            'Consumer Secret',
+            array( $this, 'consumer_secret_callback' ),
+            'twitterfeed-admin',
+            'twitter_api_keys'
+        );
+    }
+
+    /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input )
+    {
+        $new_input = array();
+
+        if( isset( $input['consumer_key'] ) )
+            $new_input['consumer_key'] = sanitize_text_field( $input['consumer_key'] );
+
+        if( isset( $input['consumer_secret'] ) )
+            $new_input['consumer_secret'] = sanitize_text_field( $input['consumer_secret'] );
+
+        return $new_input;
+    }
+
+    /**
+     * Print the Section text
+     */
+    public function twitter_api_keys_callback()
+    {
+        print '<p>Enter your API keys below (found at https://dev.twitter.com/apps):</p>';
+    }
+
+    /**
+     * Get the settings option array and print one of its values
+     */
+    public function consumer_key_callback()
+    {
+        printf(
+            '<input type="text" id="consumer_key" class="regular-text" name="twitterfeed_settings[consumer_key]" value="%s" />',
+            isset( $this->options['consumer_key'] ) ? esc_attr( $this->options['consumer_key']) : ''
+        );
+    }
+
+    /**
+     * Get the settings option array and print one of its values
+     */
+    public function consumer_secret_callback()
+    {
+        printf(
+            '<input type="text" id="consumer_secret" class="regular-text" name="twitterfeed_settings[consumer_secret]" value="%s" />',
+            isset( $this->options['consumer_secret'] ) ? esc_attr( $this->options['consumer_secret']) : ''
+        );
+    }
+
+}
+
+if( is_admin() )
+    $twitterfeed_settings_page = new TwitterFeedSettings();
---
+++ twitter-feed-shortcode.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Plugin Name: Twitter Feed Shortcode
+ * Description: A simple twitter feed shortcode using Twitter API 1.1.
+ * Version: 0.1
+ * Author: Justin Hebb
+ * Author URI: http://jukah.com
+ */
+
+include_once('class-wp-twitter-api.php');
+include_once('twitter-feed-settings.php');
+
+
+/**
+ * Display Tweets Shortcode
+ *
+ * Shortcode to display tweets using twitter api 1.1
+ *
+ * @param $account string optional - name of twitter account.
+ * @param $count int optional - number of tweets to display.
+ * @param $exclude_replies boolean - show or hide replies.
+ * @param $includ_rts boolean - show or hide retweets.
+ * @param $add_links optional boolean - Add links to hashtags and names
+ * @param $key int optional - your public twitter api key.
+ * @param $secret int optional - your secret twitter api key.
+ * Usage: [twitterfeed account="twitteraccount" count="5" exclude_replies="false" includ_rts="true" add_links=true key="your-public-key" secret="your-secret-key"]
+ */
+function shortcode_twitter_feed($atts) {
+
+	extract( shortcode_atts( array(
+		'account' => '',
+        'count' => 5,
+        'exclude_replies' => false,
+        'include_rts' => true,
+        'add_links' => true,
+        'key' => '',
+        'secret' => ''
+    ), $atts ) );
+
+	// Fix SSL issues for WordPress (https://plus.google.com/107110219316412982437/posts/gTdK4MrnKUa)
+	add_filter( 'https_ssl_verify', '__return_false' );
+	add_filter( 'https_local_ssl_verify', '__return_false' );
+
+
+	// Set access keys
+	$twittersettings = get_option('twitterfeed_settings');
+
+	$credentials = array(
+	  'consumer_key' => $twittersettings['consumer_key'],
+	  'consumer_secret' => $twittersettings['consumer_secret']
+	);
+
+
+	// Set query variables
+	$twitter_link = 'http://twitter.com/' . $account;
+
+	// Let's instantiate Wp_Twitter_Api with your credentials
+	if ( $credentials['consumer_key'] && $credentials['consumer_secret'] ) {
+		$twitter_api = new Wp_Twitter_Api( $credentials );
+	} else {
+		return 'Please check your credentials!<br />';
+	}
+
+	// Set up query
+	$query = 'count=' .$count. '&include_entities=true&exclude_replies=' . $exclude_replies . '&include_rts=' . $include_rts . '&screen_name=' .$account;
+
+
+	// Compile the results
+	$tweets = $twitter_api->query( $query );
+	$result = '';
+
+	// print_r($tweets);
+
+	if ( !is_null($tweets) && is_array($tweets) ) {
+		$tweet_display = '';
+		foreach ( $tweets as $tweet ) {
+		    /* Human friendly times  */
+			$timerange = 60*60*24*5; // one week in seconds
+			if ( ( current_time('timestamp') - strtotime( $tweet->created_at ) ) < $timerange ) {
+				$tweet_time = 'about '.human_time_diff( strtotime( $tweet->created_at ), current_time('timestamp') ) . ' ago';
+			} else {
+				$tweet_time = date( "F jS, g:i a", strtotime( $tweet->created_at ) );
+			}
+			// Tweet permalink
+		    $tweet_permalink = str_replace(
+				array(
+			        '%screen_name%',
+			        '%id%',
+			        '%created_at%'
+			    ),
+			    array(
+			        $tweet->user->screen_name,
+			        $tweet->id_str,
+			        $tweet_time,
+			    ),
+			        '<span class="twitter-feed-tweet-time"><a href="https://twitter.com/%screen_name%/status/%id%" target="_blank">%created_at%</a></span>'
+			);
+
+			// Add links to hashtags and names
+		    if ($add_links == 'true') {
+			    // linkify @names
+			    $tweet->text = preg_replace('!@([a-z0-9_]{1,15})!i', '<a class="twitter-screen-name" href="https://twitter.com/\\1" target="_blank">\\0</a>', $tweet->text );
+			    // linkify #hashtags
+			    $tweet->text = preg_replace('/(?<!&)#(\w+)/i', '<a class="twitter-hashtag" href="https://twitter.com/search?q=%23\\1&amp;src=hash" target="_blank">\\0</a>', $tweet->text );
+		    }
+
+		    // Compile single tweet markup
+	    	$tweet_display .= '<div class="twitter-feed-tweet">';
+			$tweet_display .= $tweet_permalink;
+			$tweet_display .= $tweet->text;
+			$tweet_display .= '</div>';
+		}
+
+
+		// Compile feed markup
+		$result .= '<div class="twitter-feed-wrap">';
+		//$result .= '<a href="'. $twitter_link . '"><div class="twitter-feed-title">twitter</div></a>';
+		$result .= '<div class="twitter-feed">' .$tweet_display. '</div>';
+		$result .= '</div>';
+
+	} else {
+
+		$result .= 'No tweets available!<br />';
+	}
+
+
+	// Return markup and results
+	return $result;
+}
+add_shortcode("twitterfeed", "shortcode_twitter_feed");
