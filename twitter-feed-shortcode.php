352cedd2cbda82efd0ebc9429cf0ba8e5252202c
Justin Hebb <justin@jukah.com>
2016-08-29 21:32:57 -0300

    update shortcode formatting and add image embeds

--- twitter-feed-shortcode.php
+++ twitter-feed-shortcode.php
@@ -7,8 +7,8 @@
  * Author URI: http://jukah.com
  */
 
-include_once('class-wp-twitter-api.php');
-include_once('twitter-feed-settings.php');
+include_once( 'class-wp-twitter-api.php' );
+include_once( 'twitter-feed-settings.php' );
 
 
 /**
@@ -24,37 +24,38 @@ include_once('twitter-feed-settings.php');
  *
  * Usage: [twitterfeed account="twitteraccount" count="5" exclude_replies="false" include_rts="true" add_links=true cache=1800]
  */
-function shortcode_twitter_feed($atts) {
+function shortcode_twitter_feed( $atts ) {
 
-	extract( shortcode_atts( array(
-        'account'           => '',
-        'count'             => 5,
-        'exclude_replies'   => false,
-        'include_rts'       => true,
-        'add_links'         => true,
-        'cache'             => 60 * 30
-    ), $atts ) );
+	$atts = shortcode_atts( array(
+		'account'           => '',
+		'count'             => 5,
+		'exclude_replies'   => false,
+		'include_rts'       => true,
+		'add_links'         => true,
+		'show_images'		=> true,
+		'cache'             => 60 * 30,
+	), $atts );
 
 	// Fix SSL issues for WordPress (https://plus.google.com/107110219316412982437/posts/gTdK4MrnKUa)
 	add_filter( 'https_ssl_verify', '__return_false' );
 	add_filter( 'https_local_ssl_verify', '__return_false' );
 
 	// Set access keys
-	$twittersettings = get_option('twitterfeed_settings');
+	$twittersettings = get_option( 'twitterfeed_settings' );
 
 	// Check credentials
 	$credentials = array(
-	  'consumer_key' => $twittersettings['consumer_key'],
-	  'consumer_secret' => $twittersettings['consumer_secret']
+		'consumer_key' => $twittersettings['consumer_key'],
+		'consumer_secret' => $twittersettings['consumer_secret'],
 	);
 
 	// Check twitter account
-	if ( !$account ) {
+	if ( ! $atts['account'] ) {
 		return 'Please check your account!<br />';
 	}
 
 	// Set query & arg variables
-	$twitter_link = 'http://twitter.com/' . $account;
+	$twitter_link = 'http://twitter.com/' . $atts['account'];
 
 	// Let's instantiate Wp_Twitter_Api with your credentials
 	if ( $credentials['consumer_key'] && $credentials['consumer_secret'] ) {
@@ -64,20 +65,20 @@ function shortcode_twitter_feed($atts) {
 	}
 
 	// Set up query
-	$query = 'count=' .$count. '&include_entities=true&exclude_replies=' . $exclude_replies . '&include_rts=' . $include_rts . '&screen_name=' .$account;
+	$query = 'count=' . $atts['count'] . '&include_entities=true&exclude_replies=' . $atts['exclude_replies'] . '&include_rts=' . $atts['include_rts'] . '&screen_name=' . $atts['account'];
 
 	// Add arguments
-	$args = array (
-	  'cache' => $cache
+	$args = array(
+		'cache' => $atts['cache'],
 	);
 
 	// Get the results
 	$tweets = $twitter_api->query( $query, $args );
 
 	// Return the feed markup
-	return twitter_feed_markup($tweets, $add_links);
+	return twitter_feed_markup( $tweets, $atts['add_links'], $atts['show_images'] );
 }
-add_shortcode("twitterfeed", "shortcode_twitter_feed");
+add_shortcode( 'twitterfeed', 'shortcode_twitter_feed' );
 
 
 
@@ -88,72 +89,75 @@ add_shortcode("twitterfeed", "shortcode_twitter_feed");
  *
  * @param $tweets array required - an array containing the tweets.
  * @param $add_links boolean optional - add markup for hastags and names.
+ * @param $show_images boolean optional - add markup to show embedded images.
  *
  */
-function twitter_feed_markup($tweets, $add_links = true) {
+function twitter_feed_markup( $tweets, $add_links = true, $show_images = true ) {
 
-	if ( !is_null($tweets) && is_array($tweets) ) {
+	if ( ! is_null( $tweets ) && is_array( $tweets ) ) {
 		$tweet_display = '';
 		foreach ( $tweets as $tweet ) {
-		    // Human friendly times
-			$timerange = 60*60*24*7; // one week in seconds
-			if ( ( current_time('timestamp') - strtotime( $tweet->created_at ) ) < $timerange ) {
-				$tweet_time = 'about '.human_time_diff( strtotime( $tweet->created_at ), current_time('timestamp') ) . ' ago';
+			// Human friendly times
+			$timerange = 60 * 60 * 24 * 7; // one week in seconds
+			if ( ( current_time( 'timestamp' ) - strtotime( $tweet->created_at ) ) < $timerange ) {
+				$tweet_time = 'about ' . human_time_diff( strtotime( $tweet->created_at ), current_time( 'timestamp' ) ) . ' ago';
 			} else {
-				$tweet_time = date( "F jS, g:i a", strtotime( $tweet->created_at ) );
+				$tweet_time = date( 'F jS, g:i a', strtotime( $tweet->created_at ) );
 			}
 
 			// Tweet permalink
-		    $tweet_permalink = str_replace(
+			$tweet_permalink = str_replace(
 				array(
-			        '%screen_name%',
-			        '%id%',
-			        '%created_at%'
-			    ),
-			    array(
-			        $tweet->user->screen_name,
-			        $tweet->id_str,
-			        $tweet_time,
-			    ),
-			        '<span class="twitter-feed-tweet-time"><a href="https://twitter.com/%screen_name%/status/%id%" target="_blank">%created_at%</a></span>'
+					'%screen_name%',
+					'%id%',
+					'%created_at%',
+				),
+				array(
+					$tweet->user->screen_name,
+					$tweet->id_str,
+					$tweet_time,
+				),
+				'<span class="twitter-feed-tweet-time"><a href="https://twitter.com/%screen_name%/status/%id%" target="_blank">%created_at%</a></span>'
 			);
 
 			// Add links to hashtags & names & shortlinks
-		    if ($add_links == 'true') {
-			    // linkify @names
-			    $tweet->text = preg_replace('!@([a-z0-9_]{1,15})!i', '<a class="twitter-screen-name" href="https://twitter.com/$1" target="_blank">$0</a>', $tweet->text );
+			if ( 'true' == $add_links ) {
+				// linkify @names
+				$tweet->text = preg_replace( '!@([a-z0-9_]{1,15})!i', '<a class="twitter-screen-name" href="https://twitter.com/$1" target="_blank">$0</a>', $tweet->text );
 
-			    // linkify #hashtags
-			    $tweet->text = preg_replace('/(?<!&)#(\w+)/i', '<a class="twitter-hashtag" href="https://twitter.com/search?q=%23$1&amp;src=hash" target="_blank">$0</a>', $tweet->text );
+				// linkify #hashtags
+				$tweet->text = preg_replace( '/(?<!&)#(\w+)/i', '<a class="twitter-hashtag" href="https://twitter.com/search?q=%23$1&amp;src=hash" target="_blank">$0</a>', $tweet->text );
 
-			    // linkify t.co links
-				foreach ($tweet->entities->urls as $url) {
-			        $tweet->text = str_replace($url->url, '<a class="twitter-shortlink" href="' . $url->expanded_url . '" target="_blank">' . $url->url . '</a>', $tweet->text);
-			    }
+				// linkify t.co links
+				foreach ( $tweet->entities->urls as $url ) {
+					$tweet->text = str_replace( $url->url, '<a class="twitter-shortlink" href="' . $url->expanded_url . '" target="_blank">' . $url->url . '</a>', $tweet->text );
+				}
 
-			    // linkify t.co links for media - check for the media entity first as it's not always set
-			    if ( isset($tweet->entities->media)) {
-					foreach ($tweet->entities->media as $themedia) {
-				        $tweet->text = str_replace($themedia->url, '<a class="twitter-shortlink" href="' . $themedia->expanded_url . '" target="_blank">' . $themedia->url . '</a>', $tweet->text);
-				    }
-			    }
+				// linkify t.co links for media - check for the media entity first as it's not always set
+				if ( isset( $tweet->entities->media ) ) {
+					foreach ( $tweet->entities->media as $themedia ) {
+						if ( $themedia->media_url && $show_images ) {
+							$tweet->text = str_replace( $themedia->url, '<a class="twitter-shortlink" href="' . $themedia->expanded_url . '" target="_blank"><img class="twitter-feed-media" src="' . $themedia->media_url . '"/></a>', $tweet->text );
+						} else {
+							$tweet->text = str_replace( $themedia->url, '<a class="twitter-shortlink" href="' . $themedia->expanded_url . '" target="_blank">' . $themedia->url . '</a>', $tweet->text );
+						}
+					}
+				}
 			}
 
-		    // Compile single tweet markup
-	    	$tweet_display .= '<div class="twitter-feed-tweet">';
+			// Compile single tweet markup
+			$tweet_display .= '<div class="twitter-feed-tweet">';
 			$tweet_display .= $tweet->text . ' '; // extra space between text and date
 			$tweet_display .= $tweet_permalink;
 			$tweet_display .= '</div>';
 		}
 
-
 		// Compile feed markup
 		$result = '<div class="twitter-feed-wrap">';
-		$result .= '<div class="twitter-feed">' .$tweet_display. '</div>';
+		$result .= '<div class="twitter-feed">' . $tweet_display . '</div>';
 		$result .= '</div>';
 
 	} else {
-
 		$result = 'No tweets available!<br />';
 	}
 
